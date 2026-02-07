795584708fe3bb8577b5598b31d0eeb479c59c5f
milty90 <156122688+milty90@users.noreply.github.com>
2026-02-07 08:47:35 +0100

    refactor pricing to use integer values and update logic

--- script.js
+++ script.js
@@ -1,98 +1,67 @@
 const items = [
-  { name: "Kaffee", price: "3.50" },
-  { name: "Tee", price: "2.50" },
-  { name: "Apfelschorle", price: "3.00" },
-  { name: "Wasser", price: "2.00" },
-  { name: "Bratwurst", price: "5.80" },
-  { name: "Pommes", price: "2.50" },
-  { name: "Currywurst", price: "6.20" },
-  { name: "Salat", price: "4.50" },
-  { name: "Kuchen", price: "3.80" },
-  { name: "Berliner", price: "2.80" },
-  { name: "Muffin", price: "2.50" },
-  { name: "Waffel", price: "3.20" },
-  { name: "Schnitzel", price: "8.50" },
-  { name: "Brötchen", price: "1.00" },
-  { name: "Brezel", price: "1.20" },
-  { name: "Leberkäse", price: "4.00" },
+  { name: "Kaffee", price: 350 },
+  { name: "Tee", price: 250 },
+  { name: "Apfelschorle", price: 300 },
+  { name: "Wasser", price: 200 },
+  { name: "Bratwurst", price: 580 },
+  { name: "Pommes", price: 250 },
+  { name: "Currywurst", price: 650 },
+  { name: "Salat", price: 450 },
+  { name: "Kuchen", price: 380 },
+  { name: "Berliner", price: 280 },
+  { name: "Muffin", price: 250 },
+  { name: "Waffel", price: 320 },
+  { name: "Schnitzel", price: 850 },
+  { name: "Brötchen", price: 100 },
+  { name: "Brezel", price: 120 },
+  { name: "Leberkäse", price: 400 },
 ];
 
-const checkout = (sum, isDelivery, modalCallback, clearOrderCallback) => {
-  const deliveryFee = 2.5;
-  const totalSum = isDelivery ? sum + deliveryFee : sum;
-  const deliveryText = isDelivery ? "Ihre Bestellung" : "Ihren Einkauf";
+let total = 0;
 
-  if (sum === 0) {
-    modalCallback("Achtung!", "Bitte fügen Sie Artikel hinzu.");
-    return;
-  }
+const menuContainer = document.getElementById("inputGroup");
+const orderList = document.getElementById("orderList");
+const resultContainer = document.getElementById("totalPriceDisplay");
+const clearButton = document.getElementById("resetButton");
+const onSiteButton = document.getElementById("onSiteButton");
+const deliveryButton = document.getElementById("deliveryButton");
+const modal = document.getElementById("myModal");
+const closeModalButton = document.getElementById("close-button");
 
-  if (isDelivery && sum < 20) {
-    modalCallback(
-      "Achtung!",
-      `Der Mindestbestellwert für eine Lieferung beträgt 20 €. 
-      Ihr aktueller Bestellwert ist ${sum.toFixed(2)} €. Bitte fügen Sie weitere Artikel hinzu.`,
-    );
-    return;
-  }
+function openModal(title, message) {
+  modal.style.display = "block";
+  const modalTitle = document.getElementById("modal-title");
+  modalTitle.textContent = title;
+  const modalContent = document.getElementById("modal-content");
+  modalContent.textContent = message;
+}
 
-  modalCallback(
-    "Quittung!",
-    `Vielen Dank für ${deliveryText} im Wert von ${totalSum.toFixed(2)} €. ${
-      isDelivery
-        ? `Die Liefergebühr von ${deliveryFee.toFixed(2)} € ist enthalten.`
-        : ""
-    }`,
-  );
+function closeModal() {
+  modal.style.display = "none";
+}
 
-  clearOrderCallback();
+const updateTotalDisplay = () => {
+  resultContainer.textContent = `${(total / 100).toFixed(2)} €`;
 };
 
-document.addEventListener("DOMContentLoaded", function () {
-  const menuContainer = document.getElementById("inputGroup");
-  const orderList = document.getElementById("orderList");
-  const resultContainer = document.getElementById("totalPriceDisplay");
-  const clearButton = document.getElementById("resetButton");
-  const onSiteButton = document.getElementById("onSiteButton");
-  const deliveryButton = document.getElementById("deliveryButton");
+const clearOrder = () => {
+  orderList.innerHTML = "";
+  total = 0;
+  updateTotalDisplay();
+  console.log("Order cleared");
+};
 
-  const modal = document.getElementById("myModal");
-  const closeModalButton = document.getElementById("close-button");
-
-  function openModal(title, message) {
-    modal.style.display = "block";
-    const modalTitle = document.getElementById("modal-title");
-    modalTitle.textContent = title;
-    const modalContent = document.getElementById("modal-content");
-    modalContent.textContent = message;
-  }
-
-  function closeModal() {
-    modal.style.display = "none";
-  }
-
-  let total = 0;
-
-  const updateTotalDisplay = () => {
-    resultContainer.textContent = `${total.toFixed(2)} €`;
-  };
-
-  const clearOrder = () => {
-    orderList.innerHTML = "";
-    total = 0;
-    updateTotalDisplay();
-    console.log("Order cleared");
-  };
-
+function renderMenu() {
   items.forEach((item) => {
     const menuItem = `<div class="inputItem">
             <span class="itemLabel">${item.name}</span>
-            <span class="itemPrice">${item.price} €</span>
+            <span class="itemPrice">${(item.price / 100).toFixed(2)} €</span>
           </div>`;
-
     menuContainer.innerHTML += menuItem;
   });
+}
 
+function calulateTotal() {
   menuContainer.addEventListener("click", (event) => {
     const childrenItems = [...menuContainer.children];
     console.log(...menuContainer.children);
@@ -108,27 +77,61 @@ document.addEventListener("DOMContentLoaded", function () {
 
     const orderElement = `<div class="orderItem">
             <p class="orderLabel">${selectedItem.name}: 
-            ${selectedItem.price} €</p>
+            ${(selectedItem.price / 100).toFixed(2)} €</p>
             <div class="orderLine"></div>
           </div>`;
 
     orderList.innerHTML += orderElement;
 
-    const priceValue = parseFloat(selectedItem.price);
+    const priceValue = selectedItem.price;
     total += priceValue;
     console.log(total);
     updateTotalDisplay();
   });
+}
 
-  clearButton.addEventListener("click", clearOrder);
+const checkout = (sum, isDelivery, modalCallback, clearOrderCallback) => {
+  const deliveryFee = 250;
+  const totalSum = isDelivery ? sum + deliveryFee : sum;
+  const deliveryText = isDelivery ? "Ihre Bestellung" : "Ihren Einkauf";
 
-  closeModalButton.addEventListener("click", closeModal);
+  if (sum === 0) {
+    modalCallback("Achtung!", "Bitte fügen Sie Artikel hinzu.");
+    return;
+  }
 
-  onSiteButton.addEventListener("click", () => {
-    checkout(total, false, openModal, clearOrder);
-  });
+  if (isDelivery && sum < 2000) {
+    modalCallback(
+      "Achtung!",
+      `Der Mindestbestellwert für eine Lieferung beträgt 20 €. 
+      Ihr aktueller Bestellwert ist ${(sum / 100).toFixed(2)} €. Bitte fügen Sie weitere Artikel hinzu.`,
+    );
+    return;
+  }
 
-  deliveryButton.addEventListener("click", () => {
-    checkout(total, true, openModal, clearOrder);
-  });
+  modalCallback(
+    "Quittung!",
+    `Vielen Dank für ${deliveryText} im Wert von ${(totalSum / 100).toFixed(2)} €. ${
+      isDelivery
+        ? `Die Liefergebühr von ${(deliveryFee / 100).toFixed(2)} € ist enthalten.`
+        : ""
+    }`,
+  );
+
+  clearOrderCallback();
+};
+
+renderMenu();
+calulateTotal();
+
+clearButton.addEventListener("click", clearOrder);
+
+closeModalButton.addEventListener("click", closeModal);
+
+onSiteButton.addEventListener("click", () => {
+  checkout(total, false, openModal, clearOrder);
+});
+
+deliveryButton.addEventListener("click", () => {
+  checkout(total, true, openModal, clearOrder);
 });
